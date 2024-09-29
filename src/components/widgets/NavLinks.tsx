f8d5806bffe05a6ae5323b43f519d6cbe4ee84bc
John_Lamy <johnclamy@gmail.com>
2024-09-29 20:41:04 +0100

    create the nav links for the navbar

---
+++ src/components/widgets/NavLinks.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { navLinks } from "../layout/Navbar";
+
+const NavLinks = ({ pathName }: { pathName: string }) => {
+  return (
+    <div className="hidden md:flex items-center">
+      {navLinks.map((link) => (
+        <div key={link.title} className="ml-4 md:ml-8">
+          <Link
+            href={link.href!}
+            target={link.isOpenInNewTab ? "_blank" : "_self"}
+            className={cn(
+              "hover:text-gray-900",
+              pathName === link.href && "font-semibold"
+            )}
+          >
+            {link.title}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NavLinks;

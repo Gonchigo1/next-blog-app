7117be90129e9d36c5ba78a419e42c6466c8be47
John_Lamy <johnclamy@gmail.com>
2024-09-29 23:58:27 +0100

    Wrap the navbar with a header component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between mt-8 md:mt-16 mb-12">
+      <Link href="/">
+        <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight">
+          Bloggerz
+        </h1>
+      </Link>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
